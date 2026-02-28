4f52aec6ab3fa5b093c5bd42f391d641e62bf8db
LewisSchmidtke <lewis.schmidtke@t-online.de>
2026-02-28 15:14:39 +0100

    Adjusted function to accommodate for different spark masters

--- spark/utils/spark_session.py
+++ spark/utils/spark_session.py
@@ -1,18 +1,19 @@
 from pyspark.sql import SparkSession
 
-def create_spark_session(app_name: str = "FraudDetection") -> SparkSession:
+def create_spark_session(app_name: str = "FraudDetection", master: str = "local[*]") -> SparkSession:
     """
     Create a SparkSession object.
 
     Args:
-        app_name: Name of the session.
+        app_name (str): Name of the session.
+        master (str): Spark master URL. Use 'local[*]' for local development (default) or 'spark://spark-master:7077' when running against the Docker Spark cluster
     Returns:
         SparkSession: The created SparkSession object.
     """
     spark = (
         SparkSession.builder
         .appName(app_name)
-        .master("spark://spark-master:7077")
+        .master(master)
         .config("spark.jars.packages", "org.postgresql:postgresql:42.7.3")
         .config("spark.sql.session.timeZone", "UTC")
         .getOrCreate()
